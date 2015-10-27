df724c80ac270d156ac43e9572d4ba2b13189960
茗香墨语 <yaowen424@126.com>
2015-10-27 15:00:55 +0800

    删除了原先的代码实现现有的代码

--- app/src/main/java/com/yaowen/surfaceviewandview/MainActivity.java
+++ app/src/main/java/com/yaowen/surfaceviewandview/MainActivity.java
@@ -1,49 +1,23 @@
 package com.yaowen.surfaceviewandview;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 
 
 public class MainActivity extends AppCompatActivity {
-    private MyView myView;
-    private  int REFRESH;
-    private Handler handler=new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            if (msg.what==REFRESH){
-                myView.invalidate();
-            }
-        }
-    };
+    private MySurfaceView mySurfaceView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        myView=new MyView(this);
-        setContentView(myView);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true){
-                    try {
-                        Thread.sleep(200);
-                    }catch (InterruptedException  e){
-                        e.printStackTrace();
-                    }
-                    handler.sendEmptyMessage(REFRESH);
-                    //使用postIvalidate可以直接在线程中更新UI界面
-                    //myView.postInvalidate（）；
-                }
-            }
-        }).start();
+        mySurfaceView=new MySurfaceView(this);
+        setContentView(mySurfaceView);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        myView.r+=2;
+        mySurfaceView.r+=2;
         return super.onTouchEvent(event);
     }
 
